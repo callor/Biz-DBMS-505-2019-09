13fbf9767240df18b7f39559d0eb97091edaec52
callor <callor@callor.com>
2020-01-07 16:36:35 +0900

    gallery

---
+++ 오라클 SQl/iouser(2020-01-06-Images).sql
@@ -0,0 +1,25 @@
+DROP TABLE tbl_imges CASCADE CONSTRAINTS;
+CREATE TABLE tbl_images (
+    IMG_FILE_SEQ NUMBER PRIMARY KEY,
+    IMG_FILE_P_CODE NUMBER,
+    IMG_FILE_ORIGIN_NAME nVARCHAR2(255),
+    IMG_FILE_UPLOAD_NAME nVARCHAR2(255)
+);
+
+CREATE SEQUENCE SEQ_IMAGES 
+START WITH 1 INCREMENT BY 1;
+
+SELECT * FROM tbl_images ;
+
+DELETE FROM tbl_images ;
+DELETE FROM tbl_gallery;
+
+ALTER TABLE tbl_images DROP CONSTRAINT FK_IMAGE;
+
+-- tbl_gallery tabled의 레코드가 삭제되면
+-- 같이 tbl_images 의 연관된 레코드를 모두 삭제
+ALTER TABLE tbl_images
+ADD CONSTRAINT FK_IMAGE FOREIGN KEY (img_file_p_code) 
+REFERENCES tbl_gallery(img_seq) ON DELETE CASCADE;
+
+
